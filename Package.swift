98cd20b7f71fc7fdc0646264c578dc544990f780
Lukas Kollmer <hey@lukaskollmer.de>
2022-09-28 15:21:37 +0200

    update dependencies

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
     ],
     dependencies: [
         runtimeDependency(selecting: .standard),
-        .package(url: "https://github.com/Apodini/MetadataSystem.git", .upToNextMinor(from: "0.1.4"))
+        .package(url: "https://github.com/Apodini/MetadataSystem.git", .upToNextMinor(from: "0.1.7"))
     ],
     targets: [
         .target(
